eda35858d6fefc5c8097b55899971ca00a446ca2
glebBeliaev <flysolo.project@gmail.com>
2025-09-12 16:40:17 +0200

    lesson 4.11

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -6,6 +6,21 @@ func main() {
 	const UsdToEuro float64 = 0.85
 	const UsdToRub float64 = 85.25
 
-	EroToRub := UsdToRub / UsdToEuro
-	fmt.Println(EroToRub)
+	getUserInput()
+	converter("USD", "EUR")
+}
+
+func getUserInput() float64 {
+	var currency string
+	var amount float64
+
+	fmt.Print("Введите валюту: ")
+	fmt.Scan(&currency)
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+	return amount
+}
+
+func converter(a, b string) float64 {
+	return 0
 }
