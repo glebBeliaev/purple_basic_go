6dae075268a34ac57d64c987eeaa241462be1076
glebBeliaev <flysolo.project@gmail.com>
2025-09-15 09:38:32 +0200

    module 8

--- password/main.go
+++ password/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := [4]int{1, 2, 3, 4}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(a *[4]int) {
+	for index, value := range *a {
+		(*a)[len(a)-index-1] = value
+	}
 }
