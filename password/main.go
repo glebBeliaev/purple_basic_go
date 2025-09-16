93ae713e08d9311a016ab84648741b9e6fe09d85
glebBeliaev <flysolo.project@gmail.com>
2025-09-16 08:55:52 +0200

    module 9

--- password/main.go
+++ password/main.go
@@ -1,15 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/url"
+)
 
-func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
+type account struct {
+	login    string
+	password string
+	url      string
 }
 
-func reverse(a *[4]int) {
-	for index, value := range *a {
-		(*a)[len(a)-index-1] = value
+func (acc *account) outputData() {
+	fmt.Println("Логин:", acc.login)
+	fmt.Println("Пароль:", acc.password)
+	fmt.Println("URL:", acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	var letterRuns = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRuns[rand.Intn(len(letterRuns))]
 	}
+	acc.password = string(res)
+}
+
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("EMPTY_LOGIN")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+	newAcc := &account{
+		login:    login,
+		password: password,
+		url:      urlString,
+	}
+	if newAcc.password == "" {
+		newAcc.generatePassword(8)
+	}
+	return newAcc, nil
+}
+
+func main() {
+	login := promtData("Введите логин: ")
+	password := promtData("Введите пароль: ")
+	url := promtData("Введите url: ")
+
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Не верный формат")
+		return
+	}
+	myAccount.outputData()
+
+}
+
+func promtData(promt string) string {
+	var data string
+	fmt.Print(promt)
+	fmt.Scanln(&data)
+	return data
 }
