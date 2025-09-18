00c1c9987cf3a141d5ee3faf5e55e0c201c4f3b3
glebBeliaev <flysolo.project@gmail.com>
2025-09-18 20:19:41 +0200

    homework

--- 3-bin/file/file.go
+++ 3-bin/file/file.go
@@ -1,1 +1,33 @@
 package file
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func ReadFile(name string) ([]byte, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	if !json.Valid(data) {
+		return nil, fmt.Errorf("файл %s не является корректным JSON", name)
+	}
+	return data, nil
+}
+
+func WriteFile(content []byte, name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = file.Write(content)
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("File created successfully")
+}
